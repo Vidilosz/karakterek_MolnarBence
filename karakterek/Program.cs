2d1278c641094e8200d234b29f686a2eeac083ac
Petrik Diak <diak@petrik.hu>
2025-02-14 11:22:10 +0100

    Idáig csináltuk közösen.

--- karakterek/Program.cs
+++ karakterek/Program.cs
@@ -4,7 +4,29 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Karakter> karakterek = [];
+
+			Beolvasas("karakterek.txt", karakterek);
+			foreach (var k in karakterek)
+			{
+				Console.WriteLine(k);
+			}
+		}
+
+		static void Beolvasas(string filenev, List<Karakter> karakterek)
+		{
+			StreamReader sr = new(filenev);
+
+			sr.ReadLine();
+
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(";");
+
+				Karakter karakter = new(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
 		}
 	}
 }
